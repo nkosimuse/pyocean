b32e35849235b5755b64258eb668d09ffff7fd73
nkosimuse <nkosi.muse@noaa.gov>
2014-08-06 09:32:50 -0400

    Update getdata.py
    add getobs_tempsalt_byrange, modify getobs_tempsalt_bysite

--- modules/getdata.py
+++ modules/getdata.py
@@ -258,22 +258,47 @@ def getemolt_temp(site, input_time=[dt.datetime(1880,1,1),dt.datetime(2020,1,1)]
     
     return datet,temp
 
-def getobs_tempsalt(site,input_time):
+def getobs_tempsalt_bysite(site,input_time):
     """
-    Function written by Jim Manning and used in "modvsobs"
-    get data from url, return datetime, temperature, and start and end times
-    input_time can either contain two values: start_time & end_time OR one value:interval_days
-    and they should be timezone aware 
-    example: input_time=[dt(2003,1,1,0,0,0,0,pytz.UTC),dt(2009,1,1,0,0,0,0,pytz.UTC)]
-    """
-    #url = 'http://gisweb.wh.whoi.edu:8080/dods/whoi/emolt_sensor?emolt_sensor.SITE,emolt_sensor.YRDAY0_LOCAL,emolt_sensor.TIME_LOCAL,emolt_sensor.TEMP,emolt_sensor.DEPTH_I,emolt_sensor.SALT&emolt_sensor.SITE='
-    url = 'http://comet.nefsc.noaa.gov:8080/erddap/tabledap/eMOLT.csv?time,depth,sea_water_temperature&SITE="'+str(site)+'"&orderBy("time")'
+Function written by Jim Manning and used in "modvsobs" and "getemolt",it was modified by Huanxin
+get data from url, return depth temperature,latitude,longitude, and start and end times
+input_time can either contain two values: start_time & end_time OR one value:interval_days
+and they should be timezone aware
+example: input_time=[dt(2003,1,1,0,0,0,0,pytz.UTC),dt(2009,1,1,0,0,0,0,pytz.UTC)]
+"""
+    mintime=input_time[0].strftime('%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')  # change time format
+    maxtime=input_time[1].strftime('%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')    
+   
+    url='http://comet.nefsc.noaa.gov:8080/erddap/tabledap/eMOLT.csv?SITE,time,depth,sea_water_temperature,latitude,longitude&time>='\
+    +str(mintime)+'&time<='+str(maxtime)+'&SITE="'+str(site)+'"&orderBy("depth,time")'
     df=pd.read_csv(url,skiprows=[1])
     for k in range(len(df)):
        df.time[k]=parse(df.time[k])
-    df=df[df.time>=input_time[0]]
-    df=df[df.time<=input_time[1]]
-    return df.time.values,df.sea_water_temperature.values,df.depth.values
+
+    return df.time.values,df.sea_water_temperature.values,df.depth.values,df.SITE.values,df.latitude.values,df.longitude.values
+
+def getobs_tempsalt_byrange(gbox,depth,input_time):
+    """
+Function written by Huanxin and used in "getemolt"
+get data from url, return depth temperature,latitude,longitude, and start and end times
+gbox includes 4 values, maxlon, minlon,maxlat,minlat, example:  [-69.0,-73.0,41.0,40.82]
+depth includes bottom depth and surface depth,example:  [80,0]
+input_time can either contain two values: start_time & end_time OR one value:interval_days
+and they should be timezone aware
+example: input_time=[dt(2003,1,1,0,0,0,0,pytz.UTC),dt(2009,1,1,0,0,0,0,pytz.UTC)]
+"""
+    i_maxdepth=depth[0];i_mindepth=depth[1];lon_max=gbox[0];lon_min=gbox[1];lat_max=gbox[2];lat_min=gbox[3]
+    mintime=input_time[0].strftime('%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')  # change time format
+    maxtime=input_time[1].strftime('%Y-%m-%d'+'T'+'%H:%M:%S'+'Z')
+    url='http://comet.nefsc.noaa.gov:8080/erddap/tabledap/eMOLT.csv?SITE,time,depth,sea_water_temperature,latitude,longitude&time>='\
+    +str(mintime)+'&time<='+str(maxtime)+'&depth<='+str(i_maxdepth)+'&depth>='+str(i_mindepth)+'&latitude>='\
+    +str(lat_min)+'&latitude<='+str(lat_max)+'&longitude>='+str(lon_min)+'&longitude<='+str(lon_max)+'&orderBy("SITE,depth,time")'
+    df=pd.read_csv(url,skiprows=[1])
+    for k in range(len(df)):
+       df.time[k]=parse(df.time[k])
+    return df.time.values,df.sea_water_temperature.values,df.depth.values,df.SITE.values,df.latitude.values,df.longitude.values
+    
+
 
   
 #def get_w_depth(xi, yi, url='http://geoport.whoi.edu/thredds/dodsC/bathy/gom03_v03'):
